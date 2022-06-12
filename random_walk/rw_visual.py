1015bb80594a934973dbb770b0bf6ac457c06c83
Anderson Borges de Oliveira <andersonshermann@gmail.com>
2022-06-12 15:20:15 -0300

    Inclusão do laço while para gerar novos caminhos

--- random_walk/rw_visual.py
+++ random_walk/rw_visual.py
@@ -2,21 +2,27 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-#Cria um passeio aleatório e plota os pontos
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    #Cria um passeio aleatório e plota os pontos
+    rw = RandomWalk()
+    rw.fill_walk()
 
-plt.scatter(rw.x_value, rw.y_value, edgecolors='None', c=rw.y_value, cmap=plt.cm.Reds, s=5)
+    plt.scatter(rw.x_value, rw.y_value, edgecolors='None', c=rw.y_value, cmap=plt.cm.Reds, s=5)
 
-#Define o titulo e nome dos eixos
-plt.title('Geração e Plotagem de Valores Aleatórios', fontsize=15)
-plt.xlabel('X Value', fontsize=10)
-plt.ylabel('Y Value', fontsize=10)
+    #Define o titulo e nome dos eixos
+    plt.title('Geração e Plotagem de Valores Aleatórios', fontsize=15)
+    plt.xlabel('X Value', fontsize=10)
+    plt.ylabel('Y Value', fontsize=10)
 
-#Personaliza os dados dos eixos
-plt.tick_params(axis='both', labelsize=10)
+    #Personaliza os dados dos eixos
+    plt.tick_params(axis='both', labelsize=10)
 
-#Define os limites mínimo e máximo dos eixos
-plt.axis([-900, 900, -900, 900])
+    #Define os limites mínimo e máximo dos eixos
+    plt.axis([-900, 900, -900, 900])
 
-plt.savefig('random_walk.png', bbox_inches='tight')
+    # plt.savefig('random_walk.png', bbox_inches='tight')
+    plt.show()
+
+    keep_running = input('Gerar outro caminho?(y/n): ')
+    if keep_running == 'n':
+        break
