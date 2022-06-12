ddd604e4d75627bd688457a190cc65b4144c0553
Anderson Borges de Oliveira <andersonshermann@gmail.com>
2022-06-12 15:03:10 -0300

    segundo commit - random walk

---
+++ random_walk/random_walk.py
@@ -0,0 +1,42 @@
+from random import choice
+
+class RandomWalk():
+    """Uma classe que gera passeios aleatórios"""
+    def __init__(self, num_points=100000):
+        """Método que inicializa os atributos do passeio"""
+        self.num_points = num_points
+        """Todos os passeios começam em (0, 0)"""
+        self.x_value = [0]
+        self.y_value = [0]
+    
+    def fill_walk(self):
+        """Método que define a direção e a distância do movimento"""
+        
+        #Laço para contar self.num_points
+        while len(self.x_value) < self.num_points:
+            #Define nova direção a ser seguida e a distancia a ser percorrida
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            #Rejeita movimentos que não vão a lugar nenhum
+            if x_step == 0 and y_step == 0:
+                continue
+
+            #Calcula próximos valores de x e y
+            next_x = self.x_value[-1] + x_step
+            next_y = self.y_value[-1] + y_step
+
+            self.x_value.append(next_x)
+            self.y_value.append(next_y)
+
+
+        
+
+
+
+    
---
+++ random_walk/rw_visual.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+#Cria um passeio aleatório e plota os pontos
+rw = RandomWalk()
+rw.fill_walk()
+
+plt.scatter(rw.x_value, rw.y_value, edgecolors='None', c=rw.y_value, cmap=plt.cm.Reds, s=5)
+
+#Define o titulo e nome dos eixos
+plt.title('Geração e Plotagem de Valores Aleatórios', fontsize=15)
+plt.xlabel('X Value', fontsize=10)
+plt.ylabel('Y Value', fontsize=10)
+
+#Personaliza os dados dos eixos
+plt.tick_params(axis='both', labelsize=10)
+
+#Define os limites mínimo e máximo dos eixos
+plt.axis([-900, 900, -900, 900])
+
+plt.savefig('random_walk.png', bbox_inches='tight')
